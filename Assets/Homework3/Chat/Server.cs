4d5cdb13c99264190311e0954f2c1d1a1ef40a43
Ruffiuss <ruffiuss123@gmail.com>
2022-12-18 14:28:16 +0500

    Practice code complete

---
+++ Assets/Homework3/Chat/Server.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Homework3.Chat
+{
+    public class Server : MonoBehaviour
+    {
+        #region Fields
+
+        private const int MAX_CONNECTION = 10;
+        private int port = 5805;
+        private int hostID;
+        private int reliableChannel;
+        private bool isStarted = false;
+        private byte error;
+        List<int> connectionIDs = new List<int>();
+
+        #endregion
+
+        #region UnityMethods
+
+        private void Update()
+        {
+            if (!isStarted) return;
+            int recHostId;
+            int connectionId;
+            int channelId;
+            byte[] recBuffer = new byte[1024];
+            int bufferSize = 1024;
+            int dataSize;
+            NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out
+            channelId, recBuffer, bufferSize, out dataSize, out error);
+
+            while (recData != NetworkEventType.Nothing)
+            {
+                switch (recData)
+                {
+                    case NetworkEventType.Nothing:
+                        break;
+                    case NetworkEventType.ConnectEvent:
+                        connectionIDs.Add(connectionId);
+                        SendMessageToAll($"Player {connectionId} has connected.");
+                        Debug.Log($"Player {connectionId} has connected.");
+                        break;
+                    case NetworkEventType.DataEvent:
+                        string message = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
+                        SendMessageToAll($"Player {connectionId}: {message}");
+                        Debug.Log($"Player {connectionId}: {message}");
+                        break;
+                    case NetworkEventType.DisconnectEvent:
+                        connectionIDs.Remove(connectionId);
+                        SendMessageToAll($"Player {connectionId} has disconnected.");
+                        Debug.Log($"Player {connectionId} has disconnected.");
+                        break;
+                    case NetworkEventType.BroadcastEvent:
+                        break;
+                }
+                recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer,
+                bufferSize, out dataSize, out error);
+            }
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void StartServer()
+        {
+            NetworkTransport.Init();
+            ConnectionConfig cc = new ConnectionConfig();
+            reliableChannel = cc.AddChannel(QosType.Reliable);
+            HostTopology topology = new HostTopology(cc, MAX_CONNECTION);
+            hostID = NetworkTransport.AddHost(topology, port);
+            isStarted = true;
+        }
+
+        public void ShutDownServer()
+        {
+            if (!isStarted) return;
+            NetworkTransport.RemoveHost(hostID);
+            NetworkTransport.Shutdown();
+            isStarted = false;
+        }
+
+        public void SendMessage(string message, int connectionID)
+        {
+            byte[] buffer = Encoding.Unicode.GetBytes(message);
+            NetworkTransport.Send(hostID, connectionID, reliableChannel, buffer, message.Length *
+            sizeof(char), out error);
+            if ((NetworkError)error != NetworkError.Ok) Debug.Log((NetworkError)error);
+        }
+
+        public void SendMessageToAll(string message)
+        {
+            for (int i = 0; i < connectionIDs.Count; i++)
+            {
+                SendMessage(message, connectionIDs[i]);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Homework3/practice.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class practice : MonoBehaviour
+{
+    #region Fields
+
+    [System.Obsolete]
+    private GlobalConfig gConfig = new GlobalConfig();
+    [System.Obsolete]
+    private ConnectionConfig config = new ConnectionConfig();
+    [System.Obsolete]
+    private HostTopology topology;
+
+    private int myReliableChannelId;
+    private int myUnreliableChannelId;
+    private int connectionId;
+    private int hostId;
+
+    #endregion
+
+    #region UnityMethods
+
+    [System.Obsolete]
+    void Start()
+    {
+        //gConfig.MaxPacketSize = 500;
+        NetworkTransport.Init();// gConfig);
+
+        config.AddChannel(QosType.Reliable);
+        config.AddChannel(QosType.Unreliable);
+
+        topology = new HostTopology(config, 10);
+
+        hostId = NetworkTransport.AddHost(topology, 8888);
+
+        connectionId = NetworkTransport.Connect(hostId, "127.0.0.1", 8888, 0, out var error);
+        if ((NetworkError)error != NetworkError.Ok)
+            Debug.Log((NetworkError)error);
+
+        //NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
+        //NetworkTransport.ReceiveFromHost(recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
+
+        byte[] buffer = new byte[1] { default };
+        int bufferLength = buffer.Length;
+
+        NetworkTransport.Send(hostId, connectionId, myReliableChannelId, buffer, bufferLength, out var error2);
+        if ((NetworkError)error2 != NetworkError.Ok)
+            Debug.Log((NetworkError)error);
+
+        NetworkTransport.Disconnect(hostId, connectionId, out var error3);
+        if ((NetworkError)error3 != NetworkError.Ok)
+            Debug.Log((NetworkError)error);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+}
