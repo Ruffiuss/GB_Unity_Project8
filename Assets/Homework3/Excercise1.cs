d207b4051b73d73b8a9839b944adab2a7b0d96a0
Ruffiuss <ruffiuss123@gmail.com>
2022-12-25 23:03:55 +0500

    Homework3
    ex1 in progress

---
+++ Assets/Homework3/Excercise1.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Assets.Homework3
+{
+    public class Excercise1 : MonoBehaviour
+    {
+        #region Fields
+
+        public HW3Server CurrentServer;
+        public TMP_InputField InputField;
+        public Button ConnectButton;
+        public Button DisconnectButton;
+        public Text PlayerLogText;
+        public delegate void OnMessageReceive(object message);
+        public event OnMessageReceive onMessageReceive;
+
+        private bool _isConnected = false;
+        private int _reliableChannel;
+        private int _connectionID;
+        private int _hostID;
+        private int _port = 0;
+
+        #endregion
+
+        #region UnityMethods
+
+        void Start()
+        {
+            ConnectButton.onClick.AddListener(() =>
+            {
+                if (ConnectServer())
+                {
+                    ConnectButton.interactable = false;
+                    DisconnectButton.interactable = true;
+                    PlayerLogText.text = default;
+                    SendNameToServer(InputField.text);
+                }
+                else
+                    PlayerLogText.text = "Can`t connect to server! Check log for details";
+
+            });
+
+            DisconnectButton.onClick.AddListener(()=>
+            {
+                DisconnectServer();
+                ConnectButton.interactable = true;
+                DisconnectButton.interactable = false;
+            });
+        }
+
+        void Update()
+        {
+            if (!_isConnected)
+                return;
+
+            RecieveMessage();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public bool ConnectServer()
+        {
+            NetworkTransport.Init();
+            ConnectionConfig config = new ConnectionConfig();
+            _reliableChannel = config.AddChannel(QosType.Reliable);
+            HostTopology topology = new HostTopology(config, CurrentServer.MaxConnections);
+
+            _hostID = NetworkTransport.AddHost(topology, _port);
+            _connectionID = NetworkTransport.Connect(
+                _hostID,
+                CurrentServer.Address,
+                CurrentServer.Port,
+                0,
+                out var error
+                );
+
+            if ((NetworkError)error == NetworkError.Ok)
+            {
+                _isConnected = true;
+                return true;
+            }
+            else
+            {
+                PlayerLogText.text = ((NetworkError)error).ToString();
+                _isConnected = false;
+                return false;
+            }
+        }
+
+        public void DisconnectServer()
+        {
+            if (!_isConnected)
+                return;
+
+            NetworkTransport.Disconnect(_hostID, _connectionID, out var error);
+            Debug.Log(error);
+            _isConnected = false;
+        }
+
+        private void RecieveMessage()
+        {
+            int recHostId;
+            int connectionId;
+            int channelId;
+            byte[] recBuffer = new byte[1024];
+            int bufferSize = 1024;
+            int dataSize;
+            NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out
+            channelId, recBuffer, bufferSize, out dataSize, out var error);
+            while (recData != NetworkEventType.Nothing)
+            {
+                switch (recData)
+                {
+                    case NetworkEventType.Nothing:
+                        break;
+                    case NetworkEventType.ConnectEvent:
+                        onMessageReceive?.Invoke($"You have been connected to server.");
+                        PlayerLogText.text = $"You have been connected to server.";
+                        break;
+                    case NetworkEventType.DataEvent:
+                        string message = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
+                        onMessageReceive?.Invoke(message);
+                        PlayerLogText.text = message;
+                        break;
+                    case NetworkEventType.DisconnectEvent:
+                        _isConnected = false;
+                        ConnectButton.interactable = true;
+                        DisconnectButton.interactable = false;
+                        onMessageReceive?.Invoke($"You have been disconnected from server.");
+                        PlayerLogText.text = $"You have been disconnected from server.";
+                        break;
+                    case NetworkEventType.BroadcastEvent:
+                        break;
+                }
+                recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer,
+                bufferSize, out dataSize, out error);
+            }
+        }
+
+        private void SendNameToServer(string name)
+        {
+            if (!_isConnected)
+            {
+                PlayerLogText.text = "Unable send a message without connection to server";
+                return;
+            }
+            byte[] buffer = Encoding.Unicode.GetBytes(name);
+            NetworkTransport.Send(_hostID, _connectionID, _reliableChannel, buffer, name.Length * sizeof(char), out var error);
+            if ((NetworkError)error != NetworkError.Ok)
+                PlayerLogText.text = ((NetworkError)error).ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Homework3/HW3Server.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace Assets.Homework3
+{
+    public class HW3Server : MonoBehaviour
+    {
+        #region Fields
+
+        private const int MAX_CONNECTIONS = 10;
+
+        public Button StartServerButton;
+        public Button StopServerButton;
+        public Text ServerStateText;
+        public Text ServerLogText;
+
+        private List<int> _connectionIDs = new List<int>();
+
+        private int _reliableChannel;
+        private int _hostID;
+        private int _port = 3839;
+        private bool _isActive = false;
+
+        #endregion
+
+        #region Properties
+
+        public string Address { get => "127.0.0.1"; }
+        public int Port { get => _port; }
+        public int MaxConnections { get => MAX_CONNECTIONS; }
+
+        #endregion
+
+        #region UnityMethods
+
+        private void Start()
+        {
+            StartServerButton.onClick.AddListener(() =>
+            { 
+                StartServer();
+                StartServerButton.interactable = false;
+                StopServerButton.interactable = true;
+            });
+            StopServerButton.onClick.AddListener(() =>
+            {
+                StopServer();
+                StartServerButton.interactable = true;
+                StopServerButton.interactable = false;
+            });
+        }
+
+        private void Update()
+        {
+            if (!_isActive)
+                return;
+
+            RecieveData();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void StartServer()
+        {
+            NetworkTransport.Init();
+
+            ConnectionConfig config = new ConnectionConfig();
+            _reliableChannel = config.AddChannel(QosType.Reliable);
+            HostTopology topology = new HostTopology(config, MAX_CONNECTIONS);
+            _hostID = NetworkTransport.AddHost(topology, _port);
+            ServerStateText.text = "Active. Port:" + _port;
+            _isActive = true;
+        }
+
+        private void StopServer()
+        {
+            NetworkTransport.RemoveHost(_hostID);
+            NetworkTransport.Shutdown();
+            ServerStateText.text = "Inactive";
+            _isActive = false;
+        }
+
+        private void RecieveData()
+        {
+            var recBuffer = new byte[1024];
+            var bufferSize = recBuffer.Length;
+
+            var recievedData = NetworkTransport.Receive(
+                out var recHostID,
+                out var connectionID,
+                out var channelID,
+                recBuffer,
+                bufferSize,
+                out var dataSize,
+                out var error
+                );
+
+            ServerLogText.text = $"Recieved data type: {recievedData}";
+
+            while (recievedData != NetworkEventType.Nothing)
+            {
+                switch (recievedData)
+                {
+                    case NetworkEventType.Nothing:
+                        Debug.Log("Nothing");
+                        break;
+                    case NetworkEventType.DataEvent:
+                        Debug.Log("DataEvent");
+                        break;
+                    case NetworkEventType.ConnectEvent:
+                        Debug.Log("ConnectEvent");
+                        _connectionIDs.Add(connectionID);
+                        ServerLogText.text = $"User {connectionID} connected";
+                        break;
+                    case NetworkEventType.DisconnectEvent:
+                        Debug.Log("DisconnectEvent");
+                        _connectionIDs.Remove(connectionID);
+                        ServerLogText.text = $"User {connectionID} disconnected";
+                        break;
+                    case NetworkEventType.BroadcastEvent:
+                        Debug.Log("BroadcastEvent");
+                        break;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
